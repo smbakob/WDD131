37b81359b41956a863fae1be696ea91947c1da41
smbakob <smbakob1254@gmail.com>
2025-05-12 23:26:01 -0400

    changes

--- mission/mission.js
+++ mission/mission.js
@@ -1,14 +1,21 @@
-const themeSelector = document.querySelector('select')
+
+const themeSelector = document.querySelector('select');
 
 function changeTheme() {
-    const body = document.querySelector('body')
-    const logo = document.querySelector('.logo')
+    const body = document.querySelector('body');
+    const logos = document.querySelectorAll('.logo');
 
     if (themeSelector.value === 'dark') {
-        body.classList.add('dark')
-        logo.src = './images/byui-logo_white.png'
+        body.classList.add('dark');
+        logos.forEach(logo => logo.src = './images/byui-logo_white.png');
     } else {
-        body.classList.remove('dark')
-        logo.src = './images/byui-logo_blue.webp'
-    }}
-    themeSelector.addEventListener('change', changeTheme)
+        body.classList.remove('dark');
+        logos.forEach(logo => logo.src = './images/byui-logo_blue.webp');
+    }
+}
+
+
+themeSelector.addEventListener('change', changeTheme);
+
+
+window.addEventListener('DOMContentLoaded', changeTheme);
