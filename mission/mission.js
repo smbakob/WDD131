203b947159bfc38639e9e2d1d334755f3b89fea4
smbakob <smbakob1254@gmail.com>
2025-05-12 23:14:57 -0400

    redo of mission

--- mission/mission.js
+++ mission/mission.js
@@ -2,13 +2,13 @@ const themeSelector = document.querySelector('select')
 
 function changeTheme() {
     const body = document.querySelector('body')
-    const logo = document.querySelector('img')
+    const logo = document.querySelector('.logo')
 
     if (themeSelector.value === 'dark') {
         body.classList.add('dark')
-        logo.src = './images/byui-logo-white.png'
+        logo.src = './images/byui-logo_white.png'
     } else {
         body.classList.remove('dark')
-        logo.src = './images/byui-logo-blue.webp'
+        logo.src = './images/byui-logo_blue.webp'
     }}
     themeSelector.addEventListener('change', changeTheme)
